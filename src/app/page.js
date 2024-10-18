a5f3cf1272dd0e8caaa309f7d6876ed383623ea3
defe014 <federicoaosandon@gmail.com>
2024-10-18 08:37:15 -0300

    delete page not use

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-
+'use client'
 import Image from "next/image";
 import { CardsSection } from "./CardsSection/page";
 import {MySection} from "@/components/OtroExample";
 
 export default function Home() {
-    return (
-      
+    return (      
         <MySection />
-
-
     )
 }
